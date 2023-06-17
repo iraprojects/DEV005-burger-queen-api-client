911cd0c6b68dd60e2eebd97ab5d27bed075720cc
NatSama2 <vet.nat.c@gmail.com>
2023-06-16 20:17:42 -0400

    arreglos css vista chef

--- src/Routes/Chef.jsx
+++ src/Routes/Chef.jsx
@@ -1,24 +1,35 @@
-import '../App.css';
-import '../styles/cheforders.css';
-import Top from '../Components/Top';
-import Orders from '../Components/Orders';
-import Footer from '../Components/Footer';
+// import "../App.css";
+import "../styles/cheforders.css";
+import Top from "../Components/Top";
+import Orders from "../Components/Orders";
+import Footer from "../Components/Footer";
+import Buttons from "../Components/Button";
+import logoChef from "../assets/logo-chef.png";
 
 function Chef() {
-
-    return (
-        <>
-          <Top />
-          <div className='titulOrders'>
-      <h2>Nombre Cliente</h2>
-      <h2>Mesa</h2> 
-      <h2>Hora Ingreso</h2>
-
+  return (
+    <>
+      <div id="container-orders">
+        <Top user="Chayane" logoUser={logoChef} />
+        <div className="title-orders">
+          <h2>Nombre Cliente</h2>
+          <h2>Mesa</h2>
+          <h2>
+            Hora ingreso<br />de pedido</h2>
+        </div>
+        <Orders cliente="Chayane" mesa="Vip" ingreso="altoke" />
       </div>
-          <Orders cliente='Chayane' mesa='Vip' ingreso='altoke' />
-          <Footer />
-        </>
-      );
-    }
-    
-    export default Chef;
+
+      <Footer
+        text={
+          <>
+            <Buttons text="Ver Pedidos" /* className="footer-button" */ />
+            <Buttons text="Pedidos Listos" /* className="footer-button" */ />
+          </>
+        }
+      />
+    </>
+  );
+}
+
+export default Chef;
