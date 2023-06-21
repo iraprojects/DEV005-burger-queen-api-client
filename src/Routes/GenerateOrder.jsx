faff1dd4946f07ce7ec4b0e2e1fd5f545b71c9dc
iraprojects <kristenirazu@gmail.com>
2023-06-20 23:23:47 -0600

    correcion de nombres de chef y clientes

--- src/Routes/GenerateOrder.jsx
+++ src/Routes/GenerateOrder.jsx
@@ -4,7 +4,7 @@ import Top from "../Components/Top";
 import Footer from "../Components/Footer";
 import LogoMesero from '../assets/logo-mesero.png'
 import Buttons from '../Components/Button';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 /* import { useState } from 'react' */
 
 export default function Order() {
@@ -16,32 +16,33 @@ export default function Order() {
     };
 
     return <>
-        <Top user="Chayane" logoUser={LogoMesero} />
+        <Top user="Mesero Chayane" logoUser={LogoMesero} />
 
         <div className='gray-container'>
-            <input type="text" class='input-order' placeholder='Nombre del cliente' />
+            <input type="text" class='input-order' placeholder='Nombre del cliente' autoFocus />
 
             <div className='container'>
                 <div className='orders-container'>
-                    dentro de bloque order
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, eos itaque. Facere pariatur corrupti harum suscipit porro. Ipsam, eligendi velit? Debitis tenetur, consequuntur laborum omnis cum et accusamus aperiam magnam.
-                </div>
-
-            <div className='note-total-container'>
-                <div className='notes-container'>
-                    Notas: Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure saepe reprehenderit sapiente necessitatibus aspernatur quaerat quod officiis eaque exercitationem .
+                    <p className='p-client-food'>Sandwich de jamón y queso $10.</p>
+                    <p className='p-client-food'>Café americano $5.</p>
 
                 </div>
-                <div className='total-container'>
-                <p id='p-total'>total $50</p>
-                <Buttons 
-                    id={'btn-confirmOrder'}
-                    text={'Confirmar'}
-                    onClick={() => {console.log('orden confirmada')}}
-                    />
-            </div>
 
-            </div>
+                <div className='note-total-container'>
+                    <div className='notes-container'>
+                        Notas: Cafe sin azúcar.
+
+                    </div>
+                    <div className='total-container'>
+                        <p id='p-total'>total $15</p>
+                        <Buttons
+                            id={'btn-confirmOrder'}
+                            text={'Confirmar'}
+                            onClick={() => { console.log('orden confirmada') }}
+                        />
+                    </div>
+
+                </div>
 
             </div>
 
@@ -50,11 +51,14 @@ export default function Order() {
         <Footer otherClass={'p-return'}
             text={
                 <>
-                    <Buttons
-                        id={'btn-return'}
-                        text={"↩"}
-                        onClick={handleReturnClick}
-                        /* active={selectedPedido === 'pedidos'} */ />
+                    <Link to={'/mesero'}>
+                        <Buttons
+                            id={'btn-return'}
+                            text={"⏎"}
+                            onClick={handleReturnClick}
+                            /* active={selectedPedido === 'pedidos'} */ />
+                    </Link>
+
                 </>
             }
         />
--- src/Routes/Mesero.jsx
+++ src/Routes/Mesero.jsx
@@ -11,96 +11,103 @@ import LogoMesero from "../assets/logo-mesero.png";
 import { BarDescription } from "../Components/BarDescription";
 
 export default function Meseros() {
-  const [selectedMenu, setSelectedMenu] = useState("desayuno");
+    const [selectedMenu, setSelectedMenu] = useState("desayuno");
 
-  const handleMenuClick = (menu) => {
-    setSelectedMenu(menu);
-  };
+    const handleMenuClick = (menu) => {
+        setSelectedMenu(menu);
+    };
 
-  return (
-    <>
-      <Top user="Chayane" logoUser={LogoMesero} />
-      
-      <div className="gray-container">
-        <div className="container-h2">
-          <h2 className="text-h2">
-            {selectedMenu === "desayuno"
-              ? "Desayuno"
-              : selectedMenu === "almuerzo"
-              ? "Almuerzo"
-              : ""}
-          </h2>
-        </div>
-
-        {selectedMenu === "desayuno" ? (
-          <>
-            <BarDescription text={"Desayunos"} />
-            <FoodRow text="Café Americano" price="$5" />
-            <FoodRow text="Café con leche" price="$7" />
-            <FoodRow text="Sándwich de jamón y queso" price="$10" />
-            <FoodRow text="Jugo de frutas natural" price="$7" />
-            <Buttons text={"Generar Orden"} id={"btn-order"} />
-          </>
-        ) : selectedMenu === "almuerzo" ? (
-          <>
-            <BarDescription text={"Hamburguesas"} />
-            <FoodRow text="Hamburguesa Simple" price="$10" />
-            <FoodRow text="Hamburguesa Doble" price="$15" />
-
-            <BarDescription text={"Acompañamientos"} />
-            <FoodRow text="Aros de cebolla" price="$5" />
-            <FoodRow text="Papas Fritas" price="$5" />
-
-            <BarDescription text={"Bebidas"} />
-            <FoodRow text="Agua 500ml" price="$5" />
-            <FoodRow text="Agua 700ml" price="$7" />
-            <FoodRow text="Bebida/Gaseosa 500ml" price="$7" />
-            <FoodRow text="Bebida/Gaseosa 700ml" price="$10" />
-            <Buttons text={"Generar Orden"} id={"btn-order"} />
-          </>
-        ) : null}
-      </div>
-      {selectedMenu === 'pedidos' && (
+    return (
         <>
-          <TitleOrders titleEntrega="Hora Entrega" servido="Servido" />
-          <Orders
-          cliente="Chayane" 
-          mesa="3" 
-          ingreso="1200" 
-          entrega="5678"
-          check="" />
-        </>
-      )}
+            <Top user="Mesero Chayane" logoUser={LogoMesero} />
 
-      <Footer
-        otherClass={"p-footer-mesero"}
-        text={
-          <>
-            <Buttons
-              id={"btn-mesero"}
-              text="Desayuno"
-              onClick={() => handleMenuClick("desayuno")}
-              active={selectedMenu === "desayuno"}
+            <div className="gray-container">
+                <div className="container-h2">
+                    <h2 className="text-h2">
+                        {selectedMenu === "desayuno"
+                            ? "Desayuno"
+                            : selectedMenu === "almuerzo"
+                                ? "Almuerzo"
+                                : ""}
+                    </h2>
+                </div>
+
+                {selectedMenu === "desayuno" ? (
+                    <>
+                        <BarDescription text={"Desayunos"} />
+                        <FoodRow text="Café Americano" price="$5" />
+                        <FoodRow text="Café con leche" price="$7" />
+                        <FoodRow text="Sándwich de jamón y queso" price="$10" />
+                        <FoodRow text="Jugo de frutas natural" price="$7" />
+
+                        <Link to={'/order'}>
+                            <Buttons text={"Generar Orden"} id={"btn-order"} />
+                        </Link>
+                    </>
+                ) : selectedMenu === "almuerzo" ? (
+                    <>
+                        <BarDescription text={"Hamburguesas"} />
+                        <FoodRow text="Hamburguesa Simple" price="$10" />
+                        <FoodRow text="Hamburguesa Doble" price="$15" />
+
+                        <BarDescription text={"Acompañamientos"} />
+                        <FoodRow text="Aros de cebolla" price="$5" />
+                        <FoodRow text="Papas Fritas" price="$5" />
+
+                        <BarDescription text={"Bebidas"} />
+                        <FoodRow text="Agua 500ml" price="$5" />
+                        <FoodRow text="Agua 700ml" price="$7" />
+                        <FoodRow text="Bebida/Gaseosa 500ml" price="$7" />
+                        <FoodRow text="Bebida/Gaseosa 700ml" price="$10" />
+                        
+                        <Link to={'/order'}> 
+                            <Buttons text={"Generar Orden"} id={"btn-order"}/>
+                        </Link>
+                    </>
+                ) : null}
+            
+            {selectedMenu === 'pedidos' && (
+                <>
+                    <TitleOrders titleEntrega="Hora Entrega" servido="Servido" />
+                    <Orders
+                        cliente="Zelda"
+                        mesa="3"
+                        ingreso="1200"
+                        entrega="5678"
+                        check="" />
+                </>
+            )}
+            </div>
+
+            <Footer
+                otherClass={"p-footer-mesero"}
+                text={
+                    <>
+                        <Buttons
+                            id={"btn-mesero"}
+                            text="Desayuno"
+                            onClick={() => handleMenuClick("desayuno")}
+                            active={selectedMenu === "desayuno"}
+                        />
+                        <Buttons
+                            id={"btn-mesero"}
+                            text="Almuerzo"
+                            onClick={() => handleMenuClick("almuerzo")}
+                            active={selectedMenu === "almuerzo"}
+                        />
+                        <Buttons
+                            id={"btn-mesero"}
+                            text="Pedidos"
+                            onClick={() => handleMenuClick("pedidos")}
+                            active={selectedMenu === "pedidos"}
+                        />
+                    </>
+                }
             />
-            <Buttons
-              id={"btn-mesero"}
-              text="Almuerzo"
-              onClick={() => handleMenuClick("almuerzo")}
-              active={selectedMenu === "almuerzo"}
-            />
-            <Buttons
-              id={"btn-mesero"}
-              text="Pedidos"
-              onClick={() => handleMenuClick("pedidos")}
-              active={selectedMenu === "pedidos"}
-            />
-          </>
-        }
-      />
-    </>
-  );
+        </>
+    );
 }
 
 {
-  /* <Link to='/' className='link-out'>Cerrar Sesión</Link>  */
+    /* <Link to='/' className='link-out'>Cerrar Sesión</Link>  */
 }
