61bebbda18d4a8c505c02e3b93dceae78d631845
iraprojects <kristenirazu@gmail.com>
2023-06-29 10:41:14 -0600

    deploy

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <img className="logo" src="./src/assets/logo.png" alt="Logo" />
+      <img className="logo" src="../src/assets/logo.png" alt="Logo" />
       <h1>Burger Queen</h1>
       <h2>Inicio de Sesión</h2>
     </>
